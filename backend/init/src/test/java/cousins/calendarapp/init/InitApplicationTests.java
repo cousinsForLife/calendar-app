7925e363d8f0f436b17cb02c4e06967026ca2b24
unknown <tobias.holter@t-online.de>
2025-12-17 13:18:20 +0100

    Refactors Project structure and adds Spring backend
    Added Spring Boot backend and seperated project parts into backend and frontend subdirectories

---
+++ backend/init/src/test/java/cousins/calendarapp/init/InitApplicationTests.java
@@ -0,0 +1,13 @@
+package cousins.calendarapp.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
